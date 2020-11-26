d60cb0db2107d9108e569d692ff87053fa8b0b52
Mimi <mimi@MacBook-Air-Oj-11.local>
2020-11-26 14:23:46 +0100

    Added extra file for note

---
+++ personalProject/jStrg.java
@@ -0,0 +1,19 @@
+package personalProject;
+
+public class jStrg {
+    public static void main(String[] args) {
+        // shorthand primitive data
+        String message = "I love you, DBF" + " and you know it";
+
+        // methods for string class
+        // .length, endsWith, startsWith
+        // message.endsWith("tt") can be call here print only "message "
+        // System.out.println(message.endsWith("tt")); //got "false"
+        System.out.println(message.length());
+        System.out.println(message.indexOf("jhuysfgjhjsias"))
+
+        // Differenct between arguments and paramenters.
+
+    }
+
+};
---
+++ personalProject/learn.java
@@ -0,0 +1,34 @@
+package personalProject;
+
+import java.util.Date;
+
+import java.awt.*;
+
+public class learn {
+    public static void main(String[] args) {
+        // initializing variable with meaningfull variable names
+        byte age = 98; // short intergers
+        // int davidSalary = 976_689_999; //
+        // long viewsCount = 126_819_977_789_599L; // long integers
+        // float price = 10.99F; //
+        // char letter = 'A';
+        // boolean isFemale = true;
+        Date today = new Date();
+        //Primitives types for storing simple values
+
+        // byte mimi = 45;
+        // byte david = mimi;
+        // mimi = 67;// reasssignng this doesn't affect david. davd will be 45. both completely
+        // //independent of each other
+
+        // Reference type for complex values
+        Point point1 = new Point(x:1, y:1);
+        Point point2 = point1;
+        point1.x = 2;
+
+     //both variables are pointing to the same values. they are referencing the same vlaue.
+
+        System.out.println(point2); // sout to generate the snippet
+
+    }
+}
