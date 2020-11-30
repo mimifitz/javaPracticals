61e2ffb06f75e6932182f1344ae46385b5b0f0af
Mimi <mimi@MacBook-Air-Oj-11.local>
2020-11-30 18:51:16 +0100

    arrays examples programmed

--- personalProject/jStrg.java
+++ personalProject/jStrg.java
@@ -3,17 +3,21 @@ package personalProject;
 public class jStrg {
     public static void main(String[] args) {
         // shorthand primitive data
-        String message = "I love you, DBF" + " and you know it";
+        String message = "  I love you, DBF " + " and you know it";
 
         // methods for string class
         // .length, endsWith, startsWith
         // message.endsWith("tt") can be call here print only "message "
         // System.out.println(message.endsWith("tt")); //got "false"
         System.out.println(message.length());
-        System.out.println(message.indexOf("jhuysfgjhjsias"))
+        System.out.println(message.indexOf("jhuysfgjhjsias"));
+        System.out.println(message.toString(jhuysfgjhjsias));
+        System.out.println(message.replace(target: "s", reaplacement: "b"));
+        System.out.println(message.trim())
 
         // Differenct between arguments and paramenters.
-
+// Paramenters are the space in {}.() an arguments are the values we pass
+// into the paramenters
     }
 
 };
--- personalProject/learn.java
+++ personalProject/learn.java
@@ -2,33 +2,45 @@ package personalProject;
 
 import java.util.Date;
 
+import java.util.Arrays;
+
 import java.awt.*;
 
 public class learn {
     public static void main(String[] args) {
         // initializing variable with meaningfull variable names
-        byte age = 98; // short intergers
+        // byte age = 98; // short intergers
         // int davidSalary = 976_689_999; //
         // long viewsCount = 126_819_977_789_599L; // long integers
         // float price = 10.99F; //
         // char letter = 'A';
         // boolean isFemale = true;
-        Date today = new Date();
-        //Primitives types for storing simple values
+        // Date today = new Date();
+        // Primitives types for storing simple values
 
         // byte mimi = 45;
         // byte david = mimi;
-        // mimi = 67;// reasssignng this doesn't affect david. davd will be 45. both completely
+        // mimi = 67;// reasssignng this doesn't affect david. davd will be 45. both
+        // completely
         // //independent of each other
 
         // Reference type for complex values
-        Point point1 = new Point(x:1, y:1);
-        Point point2 = point1;
-        point1.x = 2;
+        // Point point1 = new Point(x:1, y:1);
+        // Point point2 = point1;
+        // point1.x = 2;
 
-     //both variables are pointing to the same values. they are referencing the same vlaue.
+        // both variables are pointing to the same values. they are referencing the same
+        // vlaue.
 
-        System.out.println(point2); // sout to generate the snippet
+        // System.out.println(point2); // sout to generate the snippet
 
+        // Arrays
+        int[] numbers = new int[5];// 5 is the length of items in the array. Pay attention name
+        // of variable names: plu and sing.
+        numbers[0] = 1;
+        numbers[1] = 2;
+        numbers[3] = 3;
+
+        System.out.println(Arrays.toString(numbers));
     }
 }
