21dff90ab3d00f9666cd146bbc9de0470db3468a
Mimi <mimi@MacBook-Air-Oj-11.local>
2020-12-04 19:19:52 +0100

    reading input from user

--- personalProject/numberFormat.java
+++ personalProject/numberFormat.java
@@ -1,5 +1,6 @@
 import java.numberFormat;
 import java.text.NumberFormat;
+import java.util.Scanner;
 
 public class numberFormat {
     public static void main(String[] args) {
@@ -8,8 +9,30 @@ public class numberFormat {
         // String result = currency.format(66778.89);
         // System.out.println(result); //prints 66,778.89 euros
 
-        NumberFormat percent = NumberFormat.getPercentInstance();
-        String result = percent.format(5.9);
-        System.out.println(result); // prints 590%
+        // NumberFormat percent = NumberFormat.getPercentInstance();
+        // String result = percent.format(5.9);
+        // System.out.println(result); // prints 590%
+        // OR
+        // String result = NumberFormat.getPercentInstance().format(5.9);
+        // System.out.println(result); // prints 590%
+
+        // READING USER INPUT (MUST INPORT java.util.Scanner)
+        // Scanner scanner = new Scanner(System.in);// read data from terminal window
+        // with system.in
+        // byte age = scanner.nextByte();
+        // System.out.println("What's your age" + age);
+
+        // Scanner scanner = new Scanner(System.in);// read data from terminal window
+        // with system.in
+        // System.out.print("Phone number: "); // input by user
+        // long phoneNumber = scanner.nextLong();
+        // System.out.println("What's your phone number: " + phoneNumber);
+
+        /// calling a STRING
+        // Scanner scanner = new Scanner(System.in);// read data from terminal window
+        /// with system.in
+        // System.out.print("Name: "); // input by user
+        // String name = scanner.nextLine().trim(); // chainng multiple methods
+        // System.out.println("Your name is " + name);
     }
 }
